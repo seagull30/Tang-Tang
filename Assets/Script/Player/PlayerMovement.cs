17d3f87bec8e2bc90a0a1f4c3d5e24ae5b0ae15e
seagull30 <minsang22@gmail.com>
2023-03-23 14:32:29 +0900

    Feat : 플레이어 애니메이션 추가

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.U2D;
 using UnityEngine.Windows;
 
 public class PlayerMovement : MonoBehaviour
@@ -13,17 +15,30 @@ public class PlayerMovement : MonoBehaviour
 
     private PlayerInput _input;
     private Rigidbody2D _rigidBody;
-
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
 
     void Start()
     {
         _input = GetComponent<PlayerInput>();
         _rigidBody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         Move();
+    }
+
+    private void LateUpdate()
+    {
+        _animator.SetFloat("Speed",inputVec.magnitude);
+
+         if(_input.HORIZONTAL_MOVEMENT!=0)
+        {
+            _spriteRenderer.flipX = _input.HORIZONTAL_MOVEMENT < 0;
+        }
     }
 
     private void Move()
